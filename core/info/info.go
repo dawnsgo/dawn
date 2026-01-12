6a9b89bbf90a6e3d0a23d243385b2fb3feb3b7b7
zhangxinsheng <15559019861@163.com>
2026-01-12 20:45:39 +0800

    升级到v1.0.8

--- core/info/info.go
+++ core/info/info.go
@@ -27,7 +27,7 @@ const (
 	leftBottomBorder  = "└"
 	rightBottomBorder = "┘"
 	website           = "https://github.com/orgs/dawnsgo/dawn"
-	version           = "v1.0.7"
+	version           = "v1.0.8"
 	global            = "Global"
 )
 
